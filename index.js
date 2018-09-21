56228b1d2534bcb87dde2affeafa54d3c4de0e4e
George-Ovidiu STOICA <ovytkl@gmail.com>
2018-09-21 14:15:39 +0300

    VM authentification

--- index.js
+++ index.js
@@ -17,125 +17,138 @@ const WARNING = chalk.keyword('orange');
 const BOLD = chalk.bold;
 
 async function authenticateToken(token) {
-    try {
-        var response = await axios({
-            method: 'post',
-            url: SERVER + '/api/v1/vm/remote/verify',
-            headers: { 'Content-Type': 'application/json' },
-            data: { token }
-        });
-        return response.data;
-    } catch (err) {
-        console.log(err);
-    }
+	try {
+		var response = await axios({
+			method: 'post',
+			url: SERVER + '/api/v1/vm/remote/verify',
+			headers: { 'Content-Type': 'application/json' },
+			data: token
+		});
+		return response.data;
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 async function validateClientId() {
-    try {
-        var response = await axios({
-            method: 'post',
-            url: SERVER + '/api/v1/vm/client/valid',
-            headers: { 'Content-Type': 'application/json' },
-            data: { clientId: ACTIVATION_CODE }
-        });
-    } catch (err) {
-        console.log(err);
-    }
+	try {
+		var response = await axios({
+			method: 'post',
+			url: SERVER + '/api/v1/vm/client/valid',
+			headers: { 'Content-Type': 'application/json' },
+			data: { clientId: ACTIVATION_CODE }
+		});
+		return response.data;
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 async function saveToken(token) {
-    var TOKEN = __dirname + '/' + '.token.json';
-    try {
-        await fs.writeJSON(TOKEN, { token });
-    } catch (err) {
-        console.log(err);
-    }
+	var TOKEN = __dirname + '/' + '.token.json';
+	try {
+		await fs.writeJSON(TOKEN, { token });
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 
 async function init() {
-    var exists = fs.existsSync(TOKEN);
-    if (exists) {
-        try {
-            var token = await fs.readJson(TOKEN);
-            if (token) {
-                var response = await authenticateToken(token);
-                if (response.err === 0) {
-                    ACTIVATED = true;
-                    console.log('User logged in as:', response.user);
-                    saveToken(token);
-                }
-            }
-        } catch (err) {
-            console.error(err);
-        }
+	var exists = fs.existsSync(TOKEN);
+	if (exists) {
+		try {
+			var token = await fs.readJson(TOKEN);
+			if (token) {
+				var response = await authenticateToken(token);
+				if (response.err === 0) {
+					ACTIVATED = true;
+					console.log('User logged in as:', response.user.username);
+					process.exit(0);
+				}
+			}
+		} catch (err) {
+			console.error(err);
+		}
 
-    }
+	}
 }
 
 
 async function tokenPing() {
-    if (!ACTIVATED) {
+	if (!ACTIVATED) {
 
-        var id = setInterval(async function() {
-            try {
-                var response = await axios({
-                    method: 'post',
-                    url: SERVER + '/api/v1/vm/remote/token',
-                    headers: { 'Content-Type': 'application/json' },
-                    data: { clientId: ACTIVATION_CODE }
-                });
-            } catch (err) {
-                console.log(err);
-            }
-            if (response.data.err === 0) {
-                ACTIVATED = true;
-                console.log(response.data.token);
-                saveToken(response.data.token);
-            }
-            if (ACTIVATED) {
-                console.log('Clearing interval');
-                clearInterval(id);
-            }
-        }, 5000);
+		var id = setInterval(async function() {
+			try {
+				var response = await axios({
+					method: 'post',
+					url: SERVER + '/api/v1/vm/remote/token',
+					headers: { 'Content-Type': 'application/json' },
+					data: { clientId: ACTIVATION_CODE }
+				});
+			} catch (err) {
+				console.log(err);
+			}
+			if (response.data.err === 0) {
+				ACTIVATED = true;
+				// console.log('Token:', response.data.user.token);
+				console.log('User logged in as:', response.data.user.username);
+				await saveToken(response.data.user.token);
+				process.exit(0);
+			}
+			if (ACTIVATED) {
+				console.log('Clearing interval');
+				clearInterval(id);
+				return;
+			}
+		}, 5000);
 
-    }
+	}
 }
 
 
 async function promptUser() {
-    if (!ACTIVATED) {
+	if (!ACTIVATED) {
 
-        console.log(WARNING('Virtual Machine not authenticated.'));
-        try {
-            ACTIVATION_CODE = await GENERATOR.generate();
-            await validateClientId();
-        } catch (err) {
-            console.log(err);
-        }
+		console.log(WARNING('Virtual Machine not authenticated.'));
+		try {
+			ACTIVATION_CODE = await GENERATOR.generate();
+			await validateClientId();
+		} catch (err) {
+			console.log(err);
+		}
 
-        console.log(WARNING('Activation Code:'), BOLD(ACTIVATION_CODE));
-        console.log(WARNING('Please follow this link and insert activation code to authenticate:'), BOLD(LOGINLINK));
+		console.log(WARNING('Activation Code:'), BOLD(ACTIVATION_CODE));
+		console.log(WARNING('Please follow this link and insert activation code to authenticate:'), BOLD(LOGINLINK));
 
 
-        id = setInterval(async function() {
-            console.log(WARNING('\n\nActivation Code Expired.'));
-            try {
-                ACTIVATION_CODE = await GENERATOR.generate();
-                await validateClientId();
-                console.log(WARNING('New Activation Code:'), BOLD(ACTIVATION_CODE));
-                console.log(WARNING('Please follow this link and insert activation code to authenticate:'), BOLD(LOGINLINK));
-            } catch (err) {
-                console.log(err);
-            }
-            if (ACTIVATED) {
-                clearInterval(id);
-            }
-        }, 120000);
+		var id = setInterval(async function() {
+			console.log(WARNING('\n\nActivation Code Expired.'));
+			try {
+				ACTIVATION_CODE = await GENERATOR.generate();
+				await validateClientId();
+				console.log(WARNING('New Activation Code:'), BOLD(ACTIVATION_CODE));
+				console.log(WARNING('Please follow this link and insert activation code to authenticate:'), BOLD(LOGINLINK));
+			} catch (err) {
+				console.log(err);
+			}
+			if (ACTIVATED) {
+				clearImmediate(id);
+				return;
+			}
+		}, 120000);
 
-    }
+	}
 }
 
-init();
-promptUser();
-tokenPing();
+async function start() {
+	await init();
+	promptUser();
+	tokenPing();
+}
+
+start();
+
+process.on('exit', function() {
+
+});
